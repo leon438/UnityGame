e488c3a30df39d6ed0ecca7dcf02ec21ce77edcf
Leon <altongleon@gmail.com>
2021-02-13 13:51:05 +0900

    bug...

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Speed 
     public float speed;
     public bool isTouchTop;
     public bool isTouchBottom;
@@ -12,74 +13,62 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // move
+        // Key borad
         float h = Input.GetAxisRaw("Horizontal");
-        // Horizontal (Boder Right, Border Left) collision
         if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
             h = 0;
         float v = Input.GetAxisRaw("Vertical");
-        // Vertical (Boder Top, Border Bottom) collision
-        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+        if ((isTouchTop && h == 1) || (isTouchBottom && h == -1))
             v = 0;
-        Vector3 curPos = transform.position;
-        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+        // pos
+        Vector3 curpos = transform.position;
+        // nextpos
+        Vector3 nextpos = new Vector3(h, v, 0) * speed * Time.deltaTime;
 
-        transform.position = curPos + nextPos;
+        //move
+        transform.position = curpos + nextpos;
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    //Border collision
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        // Border Physics collision Trigger Enter
         if (collision.gameObject.tag == "Border")
         {
             switch (collision.gameObject.name)
             {
-                // Top Border
                 case "Top":
                     isTouchTop = true;
                     break;
-                // Bottom Border
                 case "Bottom":
                     isTouchBottom = true;
                     break;
-                // Right Border
                 case "Right":
                     isTouchRight = true;
                     break;
-                // Left Border
                 case "Left":
                     isTouchLeft = true;
                     break;
-
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-
-        // Border Physics collision Trigger Enter
         if (collision.gameObject.tag == "Border")
         {
             switch (collision.gameObject.name)
             {
-                // Top Border
                 case "Top":
                     isTouchTop = false;
                     break;
-                // Bottom Border
                 case "Bottom":
                     isTouchBottom = false;
                     break;
-                // Right Border
                 case "Right":
                     isTouchRight = false;
                     break;
-                // Left Border
                 case "Left":
                     isTouchLeft = false;
                     break;
-
             }
         }
     }
